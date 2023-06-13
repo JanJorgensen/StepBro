d7fb7bea3b8aa0414509285a3336544b399e372d
Milton Lindof <mlindof@Nilfisk.com>
2023-06-13 10:30:37 +0200

    Change to .109

--- source/CommonVersion.cs
+++ source/CommonVersion.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("JanJorgensen")]
 [assembly: AssemblyProduct("StepBro")]
-[assembly: AssemblyVersion("0.2.5.0")]
+[assembly: AssemblyVersion("0.2.5.109")]
 
