e178534a01d3299bbcd376cb195fe0e6160c06b9
Milton Lindof <mlindof@Nilfisk.com>
2023-09-04 11:11:05 +0200

    Tests for for loops

---
+++ source/Test/StepBro.Core.Test/Execution/TestProcedure_For.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StepBro.Core.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepBroCoreTest.Parser
+{
+    [TestClass]
+    public class TestProcedure_For
+    {
+        [TestMethod]
+        public void TestProcedureForStatementWithExpression01()
+        {
+            var proc = FileBuilder.ParseProcedureExpectNoErrors(
+                """
+                int Func()
+                {
+                    var output = 200;
+                    for (output = 0; output < 100; output += 12)
+                    {
+                        output++;
+                    }
+                    return output;
+                }
+                """);
+            Assert.AreEqual(typeof(long), proc.ReturnType.Type);
+            Assert.AreEqual(0, proc.Parameters.Length);
+            object result = proc.Call();
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(long));
+            Assert.AreEqual(104L, (long)result);
+        }
+
+        [TestMethod]
+        public void TestProcedureForStatementWithExpression02()
+        {
+            var proc = FileBuilder.ParseProcedureExpectNoErrors(
+                """
+                int Func()
+                {
+                    var output = 0;
+                    for (var i = 0; i < 100; i += 12)
+                    {
+                        output += 12;
+                    }
+                    return output;
+                }
+                """);
+            Assert.AreEqual(typeof(long), proc.ReturnType.Type);
+            Assert.AreEqual(0, proc.Parameters.Length);
+            object result = proc.Call();
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(long));
+            Assert.AreEqual(108L, (long)result);
+        }
+    }
+}
