9d8a5b4c5f205df4e2e7a39c40d58e19b220ddea
Milton Lindof <mlindof@Nilfisk.com>
2023-10-30 15:33:23 +0100

    Marginal steps towards something that hopefully will work

--- additional/stepbro-vscode-debug-adapter/src/typescript/extension.ts
+++ additional/stepbro-vscode-debug-adapter/src/typescript/extension.ts
@@ -4,25 +4,16 @@ import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-    vscode.commands.executeCommand('vscode.startDebug');
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "stepbro-vscode-debug-adapter" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('stepbro-vscode-debug-adapter.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from StepBro VSCode Debug Adapter!');
-	});
-
-	context.subscriptions.push(disposable);
+export function activate(context: vscode.ExtensionContext) 
+{
+    context.subscriptions.push(vscode.commands.registerCommand('stepbro-vscode-debug.startSession', config => startSession(config)));
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+function startSession(config: any)
+{
+    console.log("We started a session!");
+	vscode.commands.executeCommand('vscode.startDebug', config);
+}
