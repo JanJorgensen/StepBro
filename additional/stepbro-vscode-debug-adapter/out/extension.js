5224003286feb7711aba66885dad09ed994b43f0
Milton Lindof <mlindof@Nilfisk.com>
2023-10-31 11:16:49 +0100

    More microsteps

--- additional/stepbro-vscode-debug-adapter/out/extension.js
+++ additional/stepbro-vscode-debug-adapter/out/extension.js
@@ -1,26 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-
+const vscode = require("vscode");
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) 
-{
+function activate(context) {
     context.subscriptions.push(vscode.commands.registerCommand('extension.stepbro-vscode-debug.getProgramName', config => {
-		return vscode.window.showInputBox({
-			placeHolder: "Please enter the name of a stepbro file in the workspace folder",
-			value: "ConsoleTest.sbs"
-		});
-	}));
-
+        return vscode.window.showInputBox({
+            placeHolder: "Please enter the name of a stepbro file in the workspace folder",
+            value: "ConsoleTest.sbs"
+        });
+    }));
     context.subscriptions.push(vscode.commands.registerCommand('stepbro-vscode-debug.startSession', config => startSession(config)));
 }
-
+exports.activate = activate;
 // This method is called when your extension is deactivated
-export function deactivate() {}
-
-function startSession(config: any)
-{
+function deactivate() { }
+exports.deactivate = deactivate;
+function startSession(config) {
     console.log("We started a session!");
-	vscode.commands.executeCommand('vscode.startDebug', config);
+    vscode.commands.executeCommand('vscode.startDebug', config);
 }
+//# sourceMappingURL=extension.js.map
