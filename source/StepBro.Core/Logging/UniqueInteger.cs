c24926659eb8df372e80e246492a01289709be31
Jan Jørgensen <36003059+JanJorgensen@users.noreply.github.com>
2023-12-22 22:27:13 +0100

    Console logging improvements when using sidekick
    Show file parsing info directly; not only the logged info.
    Use same sync object in UniqueInteger as in the logging.
    Correct some comments in the code.

--- source/StepBro.Core/Logging/UniqueInteger.cs
+++ source/StepBro.Core/Logging/UniqueInteger.cs
@@ -8,10 +8,18 @@ namespace StepBro.Core.Logging
 {
     public static class UniqueInteger
     {
+        internal static object m_sync = new object();
         private static ulong m_nextLong = 1UL;
-        public static ulong GetLong()
+        public static ulong GetLongDirectly()
         {
             return m_nextLong++;
+        }
+        public static ulong GetLongProtected()
+        {
+            lock (m_sync)
+            {
+                return m_nextLong++;
+            }
         }
     }
 }
