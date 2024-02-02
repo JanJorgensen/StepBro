15691e47331b15f5717f0a6e196778e857e9dc8e
Jan Jørgensen <36003059+JanJorgensen@users.noreply.github.com>
2024-02-02 18:17:21 +0100

    Fix re-parsing of files using 'typedef's.
    Re-use 'typedef's and 'override's file elements between parsings.
    Keep/re-use typedef definition objects between parsings,, because there are references to them in different parts of the system.
    Throw away saved root identifiers before each parsing.

--- source/StepBro.Core/ScriptData/FileElement.cs
+++ source/StepBro.Core/ScriptData/FileElement.cs
@@ -20,12 +20,20 @@ namespace StepBro.Core.ScriptData
         private string m_summary;
         private string m_docReference;
         private static int g_nextID = 1000;
-        private readonly int m_uid = g_nextID++;
+        protected readonly int m_uid = g_nextID++;
         protected PropertyBlock m_propertyBlock = null;
         private readonly List<IPartner> m_partners = new List<IPartner>();
 
         public FileElement(IScriptFile file, int line, IFileElement parentElement, string @namespace, string name, AccessModifier access, FileElementType type)
         {
+            System.Diagnostics.Debug.WriteLine(
+                "~~~~~~~~ FILE ELEMENT " +
+                type.ToString().ToUpper() + ": " +
+                m_uid.ToString() + " " +
+                ((file != null) ? file.FileName : "<no file>") + " - " +
+                (String.IsNullOrEmpty(@namespace) ? "" : @namespace) + " " +
+                name);
+
             m_parentFile = file;
             m_line = line;
             m_baseElement = null;
