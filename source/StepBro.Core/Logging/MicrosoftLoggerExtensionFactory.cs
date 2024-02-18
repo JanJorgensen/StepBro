05c557a38a01bee7ac8a624640b6d5be955dfb97
Jan Jørgensen <36003059+JanJorgensen@users.noreply.github.com>
2024-02-18 16:48:11 +0100

    Add wrapper for Microsoft.Extensions.Logging.Ilogger and update NuGet packages

---
+++ source/StepBro.Core/Logging/MicrosoftLoggerExtensionFactory.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Extensions.Logging;
+using StepBro.Core.File;
+using StepBro.Core.Tasks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepBro.Core.Logging
+{
+    public static class MicrosoftExtensionLogging // : Microsoft.Extensions.Logging.ILogger
+    {
+        static public Microsoft.Extensions.Logging.ILogger<T> AsMicrosoftExtensionLogger<T>(this ILoggerScope logger)
+        {
+            return new Wrapper<T>(logger);
+        }
+
+        private class Wrapper<T> : Microsoft.Extensions.Logging.ILogger<T>
+        {
+            private ILoggerScope m_logger;
+
+            public Wrapper(ILoggerScope logger)
+            {
+                m_logger = logger;
+            }
+
+            IDisposable Microsoft.Extensions.Logging.ILogger.BeginScope<TState>(TState state)
+            {
+                throw new NotImplementedException();
+            }
+
+            bool Microsoft.Extensions.Logging.ILogger.IsEnabled(LogLevel logLevel)
+            {
+                if (m_logger.IsDebugging) return true;
+                else
+                {
+                    switch (logLevel)
+                    {
+                        case LogLevel.Trace:
+                        case LogLevel.Debug:
+                            return false;
+                        default:
+                            return true;
+                    }
+                }
+            }
+
+            void Microsoft.Extensions.Logging.ILogger.Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            {
+                switch (logLevel)
+                {
+                    case LogLevel.Trace:
+                    case LogLevel.Debug:
+                        m_logger.LogDetail(formatter(state, exception));
+                        break;
+                    case LogLevel.Information:
+                        m_logger.Log(formatter(state, exception));
+                        break;
+                    case LogLevel.Warning:
+                        m_logger.Log("Warning!" + formatter(state, exception));
+                        break;
+                    case LogLevel.Error:
+                    case LogLevel.Critical:
+                        m_logger.LogError(formatter(state, exception));
+                        break;
+                    case LogLevel.None:
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
