b2064a574e387d5a132408b2011d0b8f22f972bb
Milton Lindof <mlindof@Nilfisk.com>
2024-04-11 10:49:11 +0200

    Create IPCPipe file

---
+++ source/StepBro.Core/IPCPipe/IPCPipe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepBro.Core.IPCPipe
+{
+    public class IPCPipe : IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
