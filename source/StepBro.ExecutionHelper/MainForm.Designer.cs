b4483bc8436f8c179cd516a7089b9f84cc584626
Milton Lindof <mlindof@Nilfisk.com>
2024-04-12 11:18:05 +0200

    Add StepBro.ExecutionHelper winforms project

---
+++ source/StepBro.ExecutionHelper/MainForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace StepBro.ExecutionHelper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(144F, 144F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            ClientSize = new Size(800, 450);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Name = "MainForm";
+            Text = "StepBro Execution Helper";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ source/StepBro.ExecutionHelper/MainForm.cs
@@ -0,0 +1,10 @@
+namespace StepBro.ExecutionHelper
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
