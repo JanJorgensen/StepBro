53587514fae265f726fe29a124cbadf59215ddb3
Jan Jørgensen <36003059+JanJorgensen@users.noreply.github.com>
2024-05-22 14:50:24 +0200

    State machine updates
    Make state machine timers automatic destroy on state change.
    Add state property 'AnyActiveStateMachines'.
    Add 'RequestStop' method.

--- source/Modules/StepBro.StateMachine/IStateMachine.cs
+++ source/Modules/StepBro.StateMachine/IStateMachine.cs
@@ -7,7 +7,7 @@ namespace StepBro.StateMachine
 {
     /// <summary>
     /// Interface to a state machine instance.
-    /// This interface is a <see cref="IDynamicStepBroObject"/>, to make it possible to get and set variables as properties on this interface.
+    /// This interface is a <see cref="IDynamicStepBroObject"/>, to make it possible to get and set defined statemachine variables as properties on this interface.
     /// </summary>
     public interface IStateMachine : IDynamicStepBroObject
     {
@@ -17,6 +17,12 @@ namespace StepBro.StateMachine
         void StartTimer([Implicit] ICallContext context, string name, TimeSpan time, bool repeating, bool currentStateOnly);
         void StartTimer([Implicit] ICallContext context, string name, DateTime time, bool currentStateOnly);
         void StopTimer([Implicit] ICallContext context, string name);
+        /// <summary>
+        /// Stop the state machime instance.
+        /// </summary>
+        /// <param name="context">Standard call context object.</param>
+        void Stop([Implicit] ICallContext context);
+
         DateTime StateEntryTime { get; }
     }
 }
