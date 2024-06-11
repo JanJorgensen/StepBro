d4af5a653b76228faa340a1490f747bd2fd3847b
Milton Lindof <mlindof@Nilfisk.com>
2024-06-11 14:09:16 +0200

    Use all instead of any so we can get rid of the !

--- source/StepBro.Core/ScriptData/ScriptFile.cs
+++ source/StepBro.Core/ScriptData/ScriptFile.cs
@@ -852,7 +852,7 @@ namespace StepBro.Core.ScriptData
             else
             {
                 // If any of the identifiers already in the list is the same as info, we do not add that to the list
-                if (!m_rootIdentifiers[name].Any(ident => ident == info))
+                if (m_rootIdentifiers[name].All(ident => ident != info))
                 {
                     m_rootIdentifiers[name].Add(info);
                 }
