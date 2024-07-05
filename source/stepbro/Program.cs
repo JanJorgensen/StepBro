54de6708172576658d943a6ce5402a65861ba9cf
Milton Lindof <mlindof@Nilfisk.com>
2024-07-05 11:08:52 +0200

    Add dot

--- source/stepbro/Program.cs
+++ source/stepbro/Program.cs
@@ -766,7 +766,7 @@ namespace StepBro.Cmd
                                                     // Write error message
                                                     if (partners.Length > 0)
                                                     {
-                                                        ConsoleWriteErrorLine($"Execution of a testlist can only be done through a partner. This testlist has the following partners: {partners}");
+                                                        ConsoleWriteErrorLine($"Execution of a testlist can only be done through a partner. This testlist has the following partners: {partners}.");
                                                     }
                                                     else
                                                     {
