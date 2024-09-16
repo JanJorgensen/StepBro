0f6a1af5d9fc8c5ecd54107260032fcfcd59e463
Jan Jørgensen <36003059+JanJorgensen@users.noreply.github.com>
2024-09-16 14:56:32 +0200

    Add UserInteraction.cs

---
+++ source/StepBro.Core/Host/UserInteraction.cs
@@ -0,0 +1,34 @@
+﻿using StepBro.Core.Api;
+using StepBro.Core.Execution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepBro.Core.Host
+{
+    public enum UserResponse
+    {
+        OK,
+        Yes,
+        No,
+        Cancel
+    }
+
+    public abstract class UserInteraction
+    {
+        public bool ShowOK { get; set; } = true;
+        public bool ShowYesNo { get; set; } = false;
+        public bool ShowCancel { get; set; } = false;
+        public abstract void AddTextBlock(string text);
+        public abstract int AddSingleSelectionList(string tag, string heading, params string[] choices);
+
+        public abstract void AddImage(System.Drawing.Image image);
+        public abstract void AddImage(string file);
+
+        public abstract UserResponse Open([Implicit] ICallContext context, TimeSpan timeout = default(TimeSpan), UserResponse defaultAnswer = UserResponse.OK);
+
+        public abstract int GetSelection([Implicit] ICallContext context, string tag);
+    }
+}
