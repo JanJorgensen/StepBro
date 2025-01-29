1bd5d309570252ec951a017c69fd8d519e7ef497
Jan Jørgensen <36003059+JanJorgensen@users.noreply.github.com>
2025-01-29 15:56:39 +0100

    Roll version to 1.0.9

--- source/CommonVersion.cs
+++ source/CommonVersion.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("SchmutStein")]
 [assembly: AssemblyProduct("StepBro")]
-[assembly: AssemblyVersion("1.0.8")]
+[assembly: AssemblyVersion("1.0.9")]
 
