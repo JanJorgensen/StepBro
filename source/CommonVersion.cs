c5075143d8db43d7d51175d4a712c785c976b9b8
Jan Jørgensen <36003059+JanJorgensen@users.noreply.github.com>
2025-02-05 09:03:55 +0100

    Bump up to version 1.1.0.

--- source/CommonVersion.cs
+++ source/CommonVersion.cs
@@ -2,5 +2,4 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("SchmutStein")]
 [assembly: AssemblyProduct("StepBro")]
-[assembly: AssemblyVersion("1.0.9")]
-
+[assembly: AssemblyVersion("1.1.0")]
